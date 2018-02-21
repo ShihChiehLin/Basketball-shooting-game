4b680658760428e08ef3ce254503acb3c726e400
SteveLin <stevelin@Stevede-MBP.fios-router.home>
2018-02-20 21:29:23 -0600

    Initial commit

---
+++ Assets/project/Scripts/ScoreArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour {
+
+	public GameObject effectObject;
+
+	// Use this for initialization
+	void Start () {
+		effectObject.SetActive (false);
+	}
+	void OnTriggerEnter (Collider otherCollider){
+		if (otherCollider.GetComponent<Ball> () != null) {
+			//Debug.Log ("Score!");
+			effectObject.SetActive(true);
+		}
+	}
+}
